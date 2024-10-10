609d8121fed5483a71095686ab9a614be2a6fcdd
Josh <jjmiklos1@gmail.com>
2024-10-10 12:28:08 -0600

    Added comments

--- generate_password.js
+++ generate_password.js
@@ -1,10 +1,10 @@
-function generatePassword(length = 12) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';
-    let password = '';
-    for (let i = 0; i < length; i++) {
-    password += characters.charAt(Math.floor(Math.random() * characters.length));
+function generatePassword(length = 12) {  //----------------------------------------------------------  Creates a function that generates a password with a length of 12 characters
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_';  //---------  A list of characters that can be used in the password
+    let password = '';   //---------------------------------------------------------------------------  Sets the variable 'password' as a string
+    for (let i = 0; i < length; i++) {  //------------------------------------------------------------  The amount of characters in the password mus tbe equal to the set length
+    password += characters.charAt(Math.floor(Math.random() * characters.length));  //-----------------  Generates a random character based on the length (a lenght of 12 will generate 12 random characters)
     }
-    return password;
+    return password;  //------------------------------------------------------------------------------  Returns the string of randomly generated characters
     }
 
 module.exports = generatePassword;
